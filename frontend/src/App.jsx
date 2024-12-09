029ea7741a94025f27937209bb9f951100545187
Shane-Dias <160272459+Shane-Dias@users.noreply.github.com>
2024-12-09 09:54:41 +0530

    Header components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,19 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Users />} />
-        <Route path="/places/new" element={<NewPlace />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/places/new" element={<NewPlace />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 };
